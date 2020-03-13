dd96b5efeb613f0a347cfb975c0527d3114aa3d2
ProsperChuks <prosperc40@gmail.com>
2020-03-13 21:25:06 +0100

    created virtual environment

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('post/<int:id>/', views.post, name='post'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
+    path('postComment/', views.coment, name='postComment'),
     path('api/', include(router.urls)),
     path('api-app/', include('rest_framework.urls', namespace='rest_framework'))
 ]
